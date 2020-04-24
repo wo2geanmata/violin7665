bd96f41dfb5b160297ff6737c0a7ae81d82bdbe6
user1 <user1@DESKTOP-OE3C9LV>
2020-04-24 11:11:47 +0900

    second_commit

--- git/src/github/Github.java
+++ git/src/github/Github.java
@@ -3,6 +3,8 @@ package github;
 public class Github {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("world");
+		System.out.println("thx");
 	}
 
 }
